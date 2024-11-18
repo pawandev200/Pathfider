c7b2cfa0c674a61e0c7b759030caf394b56e1659
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2024-11-19 02:38:16 +0530

    Update BFS.js

--- src/app/algorithms/BFS.js
+++ src/app/algorithms/BFS.js
@@ -4,15 +4,16 @@ export function BFS(grid, startCell, endCell) {
   let startTime = Date.now();
   let endTime;
 
-  // Declaring a queue (array) and visited cell to track the cell while traversing and finding the target
+  // Declaring a queue (array) and visited cells to track the traversal and target search
   let unvisitedCellsQueue = [startCell];
   let visitedCells = [];
 
-  startCell.isVisited = true; // Marking the start cell as visited
+  // Marking the start cell as visited initially
+  startCell.isVisited = true;
 
-  // If queue is not empty: loop runs as long as there are unvisited cells in the queue
+  // If the queue is not empty: loop runs as long as there are unvisited cells in the queue
   while (unvisitedCellsQueue.length > 0) {
-    let currentCell = unvisitedCellsQueue.shift(); //to process cells in FIFO order, using shift() not pop()
+    let currentCell = unvisitedCellsQueue.shift(); // to process cells in FIFO order
 
     // If currentCell is null (which shouldn't happen in normal circumstances), the function returns
     if (!currentCell) {
@@ -23,9 +24,10 @@ export function BFS(grid, startCell, endCell) {
     // Extracting cell properties for easier access
     const { col, row, cellNumber } = currentCell;
 
-    visitedCells.push(currentCell); // Marking as visited
+    // Adding the current cell to the visited list
+    visitedCells.push(currentCell);
 
-    // Checking if target is found
+    // Checking if the target cell is found
     if (cellNumber === endCell.cellNumber) {
       currentCell.isTarget = true; // Marked as the target cell
       endTime = Date.now();
@@ -33,44 +35,48 @@ export function BFS(grid, startCell, endCell) {
     }
 
     // Traversing for each neighbor (right, up, down, left) within the boundary, not a wall, and not visited
+    // Right
     if (
       col + 1 < grid[0].length &&
       !grid[row][col + 1].isWall &&
       !grid[row][col + 1].isVisited
     ) {
       grid[row][col + 1].previousCell = currentCell;
-      unvisitedCellsQueue.push(grid[row][col + 1]);  //push(), not unshift() to ensure new cells are added to the end of the queue.
-      grid[row][col + 1].isVisited = true;
+      unvisitedCellsQueue.push(grid[row][col + 1]); // Adding the cell to the queue
+      grid[row][col + 1].isVisited = true; // Marking the cell as visited
     }
 
+    // Up
     if (
       row - 1 >= 0 &&
       !grid[row - 1][col].isWall &&
       !grid[row - 1][col].isVisited
     ) {
-      grid[row - 1].previousCell = currentCell;
-      unvisitedCellsQueue.push(grid[row - 1][col]); 
-      grid[row - 1][col].isVisited = true;
+      grid[row - 1][col].previousCell = currentCell;
+      unvisitedCellsQueue.push(grid[row - 1][col]);
+      grid[row - 1][col].isVisited = true; // Marking the cell as visited
     }
 
+    // Down
     if (
       row + 1 < grid.length &&
       !grid[row + 1][col].isWall &&
       !grid[row + 1][col].isVisited
     ) {
       grid[row + 1][col].previousCell = currentCell;
-      unvisitedCellsQueue.push(grid[row + 1][col]); 
-      grid[row + 1][col].isVisited = true;
+      unvisitedCellsQueue.push(grid[row + 1][col]);
+      grid[row + 1][col].isVisited = true; // Marking the cell as visited
     }
 
+    // Left
     if (
       col - 1 >= 0 &&
       !grid[row][col - 1].isWall &&
       !grid[row][col - 1].isVisited
     ) {
       grid[row][col - 1].previousCell = currentCell;
-      unvisitedCellsQueue.push(grid[row][col - 1]); 
-      grid[row][col - 1].isVisited = true;
+      unvisitedCellsQueue.push(grid[row][col - 1]);
+      grid[row][col - 1].isVisited = true; // Marking the cell as visited
     }
   }
 
@@ -78,3 +84,4 @@ export function BFS(grid, startCell, endCell) {
   endTime = Date.now();
   return [visitedCells, endTime - startTime];
 }
+
