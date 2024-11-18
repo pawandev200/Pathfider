2d996f55721bb6aab3cdcdc90da70a0ca1031b2b
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2024-11-18 09:58:03 +0530

    Update Cell.jsx

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -20,16 +20,17 @@ const Cell = ({
     <div
       {...props}
       className={`cell lg:w-6 w-4 lg:h-6 h-4 inline-flex justify-center items-center aspect-square border-[0.1px] border-indigo-300 ${
-        isStartPoint ? "!bg-green-300" : ""   // Green background for the starting point
-      } ${isEndPoint ? "!bg-gray-200" : ""} ${ // Light gray background for the endpoint
-        isWall ? "!bg-gray-900 wall-animate" : "" // Dark gray background with animation for walls
+        isStartPoint ? "!bg-green-300" : ""   /* Green background for the starting point */
+      } ${isEndPoint ? "!bg-gray-200" : ""} ${ /* Light gray background for the endpoint */
+        isWall ? "!bg-gray-900 wall-animate" : "" /* Dark gray background with animation for walls */
       }`}
     >
       {isStartPoint ? (
         <MapPinIcon className="h-4 w-4 font-bold" /> // Pin icon for the start point
       ) : isEndPoint ? (
         <TrophyIcon className="h-4 w-4 font-bold" /> // Trophy icon for the endpoint
-      ) : null} /* No content for other cells */}
+      ) : null} 
+      {/* No content for other cells */}
     </div>
   );
 };
